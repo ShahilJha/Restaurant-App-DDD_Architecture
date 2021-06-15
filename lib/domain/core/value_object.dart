2f7fe0a821153dd4965be87cf7706872e289548e
Shahil <shahil1.jha@gmail.com>
2021-06-15 16:23:16 +0545

    package: add flash

--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -9,7 +9,7 @@ abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
 
-  ///Throws [UnexpectedValueError] containingthe [ValueFailure]
+  ///Throws [UnexpectedValueError] containing the [ValueFailure]
   T getOrCrash() {
     //id - identity - same as writing (right) => right
     return value.fold((f) => throw UnexpectedValueError(f), id);
