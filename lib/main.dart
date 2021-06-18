a8e19976454123488ad8fba5ef225168367d81d7
Shahil <shahil1.jha@gmail.com>
2021-06-18 20:39:48 +0545

    app: Firebase.initializeApp to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:restaurant_app/injection.dart';
 import 'package:restaurant_app/presentation/core/app_widget.dart';
 
-void main() {
+//TODO: undo main() async, handle the Firebase.initializeApp()
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   configureInjection(Environment.prod);
   runApp(AppWidget());
 }
--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:restaurant_app/presentation/core/app_theme.dart';
 import 'package:restaurant_app/presentation/pages/sign_in/sign_in_page.dart';
-import 'package:restaurant_app/presentation/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -14,22 +12,7 @@ class AppWidget extends StatelessWidget {
         title: 'Restaurant Application',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.generateThemeData(),
-        home: FutureBuilder(
-          future: Firebase.initializeApp(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              return SignInPage();
-            }
-            return SplashPage();
-          },
-        ),
+        home: SignInPage(),
       ),
     );
   }
