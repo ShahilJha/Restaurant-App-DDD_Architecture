1a29f141585b4def14dd83518159f53d76b60fee
Shahil <shahil1.jha@gmail.com>
2021-06-20 11:47:09 +0545

    app: make auth_bloc injectable

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
 import 'package:restaurant_app/domain/auth/i_auth_facade.dart';
 
@@ -9,15 +10,20 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
+@injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final IAuthFacade _authFacade;
 
-  AuthBloc(this._authFacade) : super(AuthState.initial());
+  AuthBloc(this._authFacade) : super(const AuthState.initial());
 
   @override
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* state.map(
+      initial: (e) async* {},
+      authenticated: (e) async* {},
+      unauthenticated: (e) async* {},
+    );
   }
 }
